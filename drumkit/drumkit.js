413d0458a93b1afb5b14a0b9b38901e836880ef1
Marco Siguencia <m08251983@gmail.com>
2018-01-14 00:29:50 -0500

    Refactoring the script
    drumkit/drumkit.js:
            - The script has been refactored to make it more readable.

--- drumkit/drumkit.js
+++ drumkit/drumkit.js
@@ -1,13 +1,7 @@
 window.onload = function () {
-    var keys = document.querySelectorAll('.key');
     //var keys = document.getElementsByClassName('key');
-    keys.forEach(function (key) {
-        key.addEventListener('transitionend', function () {
-            /*this refers to the element we registered the listener with*/
-            this.classList.remove('pressed');
-        });
-    });
-    document.addEventListener('keydown', function (event) {
+
+    function play (event) {
         var key = document.querySelector(`div[data-key="${event.keyCode}"]`);
         var audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
         if (!key) return;
@@ -15,7 +9,16 @@ window.onload = function () {
         audio.currentTime = 0;
         audio.play();
         key.classList.add('pressed');
+    }
+
+    var keys = document.querySelectorAll('.key');
+    keys.forEach(function (key) {
+        key.addEventListener('transitionend', function () {
+            /*this refers to the element we registered the listener with*/
+            this.classList.remove('pressed');
+        });
     });
-        
+
+    document.addEventListener('keydown', play);
     return;
 };
