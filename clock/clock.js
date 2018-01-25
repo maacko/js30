68740849944e2617c0aeacb4bdc5effbf550d539
Marco Siguencia <m08251983@gmail.com>
2018-01-25 15:54:24 -0500

    Moving the Minute Hand Per Second Within a 60 Minute Period
    Instead of having the minute hand move 6 degrees for every minute elapsed, we'll
    "inch" forward the hand for every second in a 60 minute period. This will give
    off a mechanical animation as if it's in sync with the seconds hand: the minute
    hand will move 360 deg in 3600seconds (3600seconds in an hour). This gives us a
    rate of 1/10th of a degree per second.  To get the correct position, we move the
    minute hand 1/10th of a degree for each of the elapsed number of second (60secs
    * current_min + current_secs).
    
    We need the current amount of seconds elapsed because this function is invoked
    every second for every minute; the expression 60seconds*current_minute will
    always give us the same result within a 60 second period because the minute
    never changes. The expression converts the number of minutes into seconds.
    Therefore, get the current total number of seconds elapsed, we'll have add the
    number of second elapsed within the span of the current minute.
    
    The expression below results in the number of degrees that the hand should move
    given the current amount of seconds elapsed within a span of an hour.
    
    I'll be sure to add the same behavior to the hour hand.
    
    I also need to figure out how to optimize this. Calculating and drawing this
    stuff this frequently has to be taxing for something that just tells time.
    
    clock/clock.js:
            - Changing the minute hand's behavior. Previously the hand would move
              to reflect the current minutes. Now we "inch" forward the hand every
              second. This gives the clock a mechanical feeling as if the minute
              hand is in sync with the seconds hand.

--- clock/clock.js
+++ clock/clock.js
@@ -27,8 +27,32 @@ window.onload = function () {
         return (360/60)*seconds;
     }
 
-    function mapMinutesToDegrees(minutes) {
-        return (360/60)*minutes;
+    function mapMinutesToDegrees(minutes,seconds) {
+        /* return (360/60)*minutes;
+         *
+         * Instead of having the minute hand move 6 degrees for every minute
+         * elapsed, we'll move it for every second in a 60 minute period. This
+         * will give us a mechanical animation that is in sync with the seconds
+         * hand: the minute hand will move 360 deg in 3600seconds (3600seconds
+         * in an hour). This gives us a rate of 1/10th of a degree per second.
+         *
+         * To get the correct position, we move the minute hand 1/10th of a
+         * degree for each of the elapsed number of second (60secs * current_min
+         * + current_secs).
+         *
+         * We need the current amount of seconds elapsed because this function
+         * is invoked every second for every minute; the expression
+         * 60seconds*current_minute will always give us the same result within a
+         * 60 second period because the minute never changes. The expression
+         * converts the number of minutes into seconds. Therefore, get the
+         * current total number of seconds elapsed, we'll have add the number of
+         * second elapsed within the span of the current minute.
+         *
+         * The expression below results in the number of degrees that the hand
+         * should move given the current amount of seconds elapsed within a span
+         * of an hour.
+         */
+        return (1/10)*(minutes*60 + seconds);
     }
 
     function mapHourToDegrees(hour) {
@@ -55,7 +79,7 @@ window.onload = function () {
         //at 90deg.
         //var degrees = mapSecondsToDegrees(totalSecondsElapsed) + 90;
         var degrees = mapSecondsToDegrees(seconds) + 90;
-        var degreesmin = mapMinutesToDegrees(minutes) + 90;
+        var degreesmin = mapMinutesToDegrees(minutes, seconds) + 90;
         var degreehour = mapHourToDegrees(hour) + 90;
         secHand.style.transform = `rotate(${degrees}deg)`;
         minHand.style.transform = `rotate(${degreesmin}deg)`;
